6e13c24a50a9c1566de38b4f36b5644a4e96e0e7
Tender <tender121995@gmail.com>
2023-03-25 19:03:21 +0200

    task-4-added-mockups-for-update-and-add-new-car

--- getcars.js
+++ getcars.js
@@ -23,20 +23,20 @@ function createCard(car) {
 
 
       <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
-          ID:${car.id}</span>
+          ID: ${car.id}</span>
       <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
-          Make:${car.make}</span>
+          Make: ${car.make}</span>
       <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
-          Model:${car.model}</span>
+          Model: ${car.model}</span>
       <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
-          Year:${car.year}</span>
+          Year: ${car.year}</span>
       <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
-          Color:${car.color}</span>
+          Color: ${car.color}</span>
       <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
-          Price:${car.price}</span>
+          Price: ${car.price}</span>
   </div>
   <div class="mt-3">
-      <a href="#" class="btn btn-primary">Update</a>
+      <a href="update.html" class="btn btn-primary">Update</a>
   </div>
 </div>
   

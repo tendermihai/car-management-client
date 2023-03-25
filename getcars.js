9b567d971f80fa0c7881d69a242a82c4b6b4c9f8
Tender <tender121995@gmail.com>
2023-03-25 17:56:21 +0200

    task-3-added-bootstrap

--- getcars.js
+++ getcars.js
@@ -6,29 +6,48 @@ async function getCars() {
 }
 
 function createCard(car) {
-  let section = document.createElement("section");
-  section.classList.add("card");
+  let div = document.createElement("div");
+  div.classList.add("col-lg-4");
 
-  section.innerHTML = `
+  div.classList.add("col-md-6");
+  div.classList.add("col-12");
+  div.classList.add("mt-4");
+  div.classList.add("pt-2");
+
+  div.innerHTML = `
   
-  <section class="card">
-  <p class="id">ID: ${car.id}</p>
-  <p class="make">Make: ${car.make}</p>
-  <p class="model">Model: ${car.model}</p>
-  <p class="year">Year: ${car.year}</p>
-  <p class="color">Color: ${car.color}</p>
-  <p class="price">Price: ${car.price} euro</p>
+  <div class="card-body p-4">
+  <span class="badge rounded-pill bg-primary float-md-end mb-3 mb-sm-0">Delete</span>
+  <h5>${car.model}</h5>
+  <div class="mt-3">
 
-</section>
+
+      <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
+          ID:${car.id}</span>
+      <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
+          Make:${car.make}</span>
+      <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
+          Model:${car.model}</span>
+      <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
+          Year:${car.year}</span>
+      <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
+          Color:${car.color}</span>
+      <span class="text-muted d-block"><i class="fa fa-car" aria-hidden="true"></i>
+          Price:${car.price}</span>
+  </div>
+  <div class="mt-3">
+      <a href="#" class="btn btn-primary">Update</a>
+  </div>
+</div>
   
   `;
 
-  return section;
+  return div;
 }
 
 function attachCards(cars) {
   console.log(cars);
-  let container = document.querySelector(".container");
+  let container = document.querySelector(".app-container");
   container.innerHTML = "";
   cars.forEach((car) => {
     container.appendChild(createCard(car));
